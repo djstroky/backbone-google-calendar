e2c85077a3ea6dd238684e62e049d68fb686c497
djstroky <djstroky@yahoo.com>
2014-12-31 16:23:51 -0800

    adding in code to use a manager to handle authentication and override Backbone.sync

--- backboneGoogleCalendar.js
+++ backboneGoogleCalendar.js
@@ -1,7 +1,74 @@
-bgcEvent = Backbone.Model.extend({
-	
-});
+var CalendarManager = CalendarManager || {};
 
-bgcCollection = Backbone.Collection.extend({
-  model: bgcEvent
-});
+CalendarManager.isAuthorized = false;
+
+CalendarManager.authorizationScopes = ['https://www.googleapis.com/auth/calendar'];
+
+CalendarManager.handleAuthenticationResut = function(authResult, callback) {
+  if(authResult && !authResult.error) {
+	  this.isAuthorized = true;
+  }
+  if(callback) {
+	  callback(this.isAuthroized, authResult);
+  }
+};
+
+CalendarManager.obtainAuthorization = function(googleApi, clientId, callback){
+  var _this = this;
+  this.gapi = googleApi;
+  googleApi.auth.authorize({ 
+    client_id: clientId, 
+    scope: this.authorizationScopes, 
+    immediate: true
+  }, function(result) {
+	  _this.handleAuthenticationResut(result, callback);
+  });
+};
+
+CalendarManager.sync = function(method, model, options){
+  switch(method) {
+    case 'read':
+      if(model.model) {
+    	  //collection read
+        this.gapi.client.load('calendar', 'v3', function() {
+    	    var request = gapi.client.calendar.events.list({
+    	      calendarId: model.calendarId,
+            timeMin: options.timeMin,
+    	      timeMax: options.timeMax
+          });
+    	          
+    	    request.then(function(resp) {
+    	      options.success(resp);
+    	    }, function(reason) {
+    	      options.failure(reason);
+    	    });
+        });
+      } else {
+    	  //model read
+      }
+	    break;
+    case 'create':
+      break;
+    case 'update':
+    case 'patch':
+	    break;
+    case 'delete':
+	    break;
+  }
+};
+
+CalendarManager.makeEventModel = function(calendarId) {
+  return Backbone.Model.extend({
+	  calendarId: calendarId,
+	  sync: this.sync
+  });
+};
+
+CalendarManager.makeCalendar = function(calendarId) {
+  var BackboneGoogleCalendar = Backbone.Collection.extend({
+	  calendarId: calendarId,
+	  model: this.makeEventModel(calendarId),
+	  sync: this.sync
+  });
+  return new BackboneGoogleCalendar();
+};
